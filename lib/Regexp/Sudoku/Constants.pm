a2fb0ca76a2bd0d72eefb5658d7d21823a75ead3
Abigail <abigail@abigail.freedom.nl>
2022-02-24 20:01:16 +0100

    More POD for Regexp::Sudoku::Constants

--- lib/Regexp/Sudoku/Constants.pm
+++ lib/Regexp/Sudoku/Constants.pm
@@ -128,3 +128,122 @@ Regexp::Sudoku::Constants - Constants related to Regexp::Sudoku
 
 =head1 DESCRIPTION
 
+This module exports constants to be used to configure Sudoku variants
+when using C<< Regexp::Sudoku >>.
+
+All constants are bitmasks based on C<< vec >>. Constants are grouped
+based on L<< Exporter >> tags; constants exported by the same tag
+can be mixed using the bitwise operators: C<< |. >>, C<< &. >>
+and C<< ~. >>.
+
+There are three tags C<< :Houses >>, C<< :Constraints >> and
+C<< :Diagonals >>. There is also the tag C<< :All >>, which can
+be used to import all the constants.
+
+We'll discuss the constants below, grouped by the tag which imports
+them. (You can still import each constant individually if you wish
+to do so).
+
+=head2 C<< :Houses >>
+
+These are used to signal the Sudoku variant uses additional houses.
+For a description of each additional house, see
+L<< Regexp::Sudoku >>.
+
+The constants are used for the C<< houses >> parameter of the
+C<< init >> function of C<< Regexp::Sudoku >>.
+
+=over 2
+
+=item C<< $NRC >>
+
+This is for I<< NRC Sudokus >>; also called I<< Windokus >> or
+I<< Hyper Sudokus >>.
+
+=item C<< $ASTERISK >>
+
+This is for I<< Asterisk Sudokus >>. 
+
+=item C<< $GIRANDOLA >>
+
+This is for I<< Girandola Sudokus >>. 
+
+=item C<< $CENTER_DOT >>
+
+This is for I<< center dot Sudokus >>.
+
+=back
+
+=head2 C<< :Constraints >>
+
+These constants are used for the C<< constraints >> parameter, and
+indicate which additionally constraints apply to the Sudoku variant.
+
+=over 2
+
+=item C<< $ANTI_KNIGHT >>
+
+In an I<< Anti-Knight Sudoku >>, cells which are a Knights move away
+(as in Chess) must be different.
+
+=item C<< $ANTI_KING >>
+
+In an I<< Anti-King Sudoku >>, cells which touch each other (including cells 
+which only touch by their corners) must be different. These cells
+corresponds with a Kings move in Chess. This type of Sudoku is also
+known as a I<< No Touch Sudoku >>.
+
+=back 2
+
+=head2 C<< :Diagonals >>
+
+=head1 BUGS
+
+There are no known bugs.
+
+=head1 SEE ALSO
+
+L<< Regexp::Sudoku >>.
+
+=head1 DEVELOPMENT
+
+The current sources of this module are found on github,
+L<< git://github.com/Abigail/Regexp-Sudoku.git >>.
+
+=head1 AUTHOR
+
+Abigail, L<< mailto:cpan@abigail.freedom.nl >>.
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (C) 2021-2022 by Abigail.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),   
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+=head1 INSTALLATION
+
+To install this module, run, after unpacking the tar-ball, the 
+following commands:
+
+   perl Makefile.PL
+   make
+   make test
+   make install
+
+=cut
